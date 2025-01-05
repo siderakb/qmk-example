3bc23fd89fbec600044446aa03e9328c80939f6b
ziteh <honmonoh@gmail.com>
2025-01-05 10:27:11 +0800

    enable double-tab rest for RP2040

--- 0_my_kb_promicro/rules.mk
+++ 0_my_kb_promicro/rules.mk
@@ -1,2 +1,3 @@
 # Processor frequency, for 3.3v/8MHz Pro Micro
+# https://docs.qmk.fm/config_options#avr-mcu-options
 F_CPU = 8000000
---
+++ 0_my_kb_rp2040/config.h
@@ -0,0 +1,3 @@
+// Enable double-tap reset mechanism
+// https://docs.qmk.fm/platformdev_rp2040#double-tap
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
